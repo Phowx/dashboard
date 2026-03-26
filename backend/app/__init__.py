cf981fc8f86784114394a36186610cb2d46e7246
Phowx <xp1108@gmail.com>
2026-03-26 10:22:58 +0800

    Fix backend import error

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,1 @@
-from .system import router as system_router
-from .docker import router as docker_router
-from .systemd import router as systemd_router
-from .alerts import router as alerts_router
-
-router = system_router
+# This file can be empty. main.py imports routers directly.
